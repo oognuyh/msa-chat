601527d358f207eaa55ee420914414dc5c6b647d
Hyungoo Kang <oognuyh@gmail.com>
2021-12-21 19:53:03 +0900

    refactor: remove unnecessary dependency

--- config-service/build.gradle.kts
+++ config-service/build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springframework.cloud:spring-cloud-config-server")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
 }
